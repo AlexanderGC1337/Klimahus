82a94abc046d49514e5042c133116210b4a66cd6
Andreas G. Christensen <48524202+Andreas-DEV@users.noreply.github.com>
2023-11-14 13:42:49 +0100

    opdatering

---
+++ src/app/house/page.tsx
@@ -0,0 +1,15 @@
+import Data from "../../../public/assets/data.json"
+
+export default function Page() {
+
+    const content = Data.content
+
+    console.log(content);
+    
+
+    return (
+        <>
+           
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Page from "./house/page";
+import Link from "next/link";
+import Data from "../../public/assets/data.json"
 
 export default function Home() {
   return (
 
-    /* HEADER */
+    <>
 
-    <header id={styles.headerContainer}>
+      <header id={styles.headerContainer}>
 
-        <img id={styles.logo} src={'https://unsplash.it/200/140'} alt="Logo" />
+        <Link id={styles.link} href={"/house"}>
 
-    </header>
+          {/* <img id={styles.logo} src={'https://unsplash.it/200/140'} alt="Logo" /> */}
+          <h2 id={styles.logo}>logo</h2>
 
-    /* HEADER END */
+        </Link>
+
+
+      </header>
+
+      <Link href={'/house'}>
+
+        <img id={styles.tinyhouse} src={'https://unsplash.it/500/500'} alt="Logo" />
+
+      </Link>
+
+      <section id={styles.infoboks}>
+        <h3 id={styles.infoboks_h3}>{Data.content.infoboks}</h3>
+      </section>
+
+    </>
+
+
 
   )
 }
